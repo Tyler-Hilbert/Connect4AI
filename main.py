4ac4d420629d2a042f5db1b76e5394c5fdf3c30d
tyler-hilbert <tylerjoshuahilbert@gmail.com>
2018-03-29 01:08:43 +0100

    Game is played with random data and it seems like the CheckGrid bug was fixed

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import numpy as np
+from random import *
+
+
+# Places piece
+# 0 means piece could not be placed
+def Place(turn, col, grid):
+	for r in range (5, -1, -1):
+		if grid[r,col] == '*':
+			grid[r,col] = turn
+			return 1
+
+		if r == 0:
+			print ("Row full")
+			return 0
+
 
 # Checks if the game is won..... a 0 means game has been won, 1 means keep playing
+# TODO - check if all positions are being checked correctly... it's likely one of the edge positions isn't being checked
 def CheckGrid(grid):
 	# Check vertical
-	for c in range (0, 5):
+	for c in range (0, 7):
 		rCount = 0;
 		yCount = 0;
 		for r in range (0, 6):
@@ -28,7 +44,7 @@ def CheckGrid(grid):
 	for r in range (0, 6):
 		rCount = 0;
 		yCount = 0;
-		for c in range (0, 5):
+		for c in range (0, 7):
 			if grid[r, c] == 'R':
 				rCount+=1
 				yCount=0
@@ -104,19 +120,24 @@ def CheckGrid(grid):
 
 	return 1
 
-grid = np.repeat(' ', 7*6).reshape(6, 7)
-grid[5,0] = 'R'
-grid[4,1] = 'Y'
-grid[4,2] = 'R'
-grid[2,3] = 'Y'
-grid[4,0] = 'R'
-grid[4,3] = 'R'
-grid[0,6] = 'Y'
-grid[1,5] = 'Y'
-grid[2,4] = 'Y'
-grid[3,3] = 'Y'
-print(grid)
-
-CheckGrid(grid)
 
 
+
+
+# TODO - init function
+# Create Grid
+grid = np.repeat('*', 7*6).reshape(6, 7)
+
+# Play game
+turn = 'R'
+print(grid, '\n')
+while (CheckGrid(grid) == 1):
+	while(Place(turn, randint(0,6), grid) == 0):
+		pass
+
+	print(grid, '\n')
+
+	if turn == 'R':
+		turn = 'Y'
+	else:
+		turn = 'R'
