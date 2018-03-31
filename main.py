9c5d194be6018c5101a2acf8e12f5f3ebf0e4fe2
tyler-hilbert <tylerjoshuahilbert@gmail.com>
2018-03-31 01:46:49 +0100

    Score tracking

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import numpy as np
 from random import *
 
 
+
+# Prints msg to console if enable is true
+def PrintIfEnabled(msg, enabled):
+	if enabled == True:
+		print(msg)
+
+
 # Places piece
 # 0 means piece could not be placed
-def Place(turn, col, grid):
+def Place(turn, col, grid, gameDisplay):
 	for r in range (5, -1, -1):
 		if grid[r,col] == '*':
 			grid[r,col] = turn
 			return 1
 
 		if r == 0:
-			print ("Row full")
+			PrintIfEnabled ("Row full", gameDisplay)
 			return 0
 
 
-# Checks if the game is won..... a R means game has been won by R, Y means game was won by Y, 1 means keep playing
+# Checks if the game is won..... a R means game has been won by R, Y means game was won by Y, 1 means keep playing, T means tie
 # TODO - check if all positions are being checked correctly... it's likely one of the edge positions isn't being checked
-def CheckGrid(grid):
+def CheckGrid(grid, gameDisplay):
 	# Check vertical
 	for c in range (0, 7):
 		rCount = 0;
@@ -34,10 +41,10 @@ def CheckGrid(grid):
 				yCount=0;
 
 			if rCount==4:
-				print("Red wins vertical");
+				PrintIfEnabled("Red wins vertical", gameDisplay);
 				return 'R'
 			elif yCount==4:
-				print("Yellow wins vertical")
+				PrintIfEnabled("Yellow wins vertical", gameDisplay)
 				return 'Y'
 
 	# Check horizontal
@@ -56,10 +63,10 @@ def CheckGrid(grid):
 				yCount=0;
 
 			if rCount==4:
-				print("Red wins horizontal");
+				PrintIfEnabled("Red wins horizontal", gameDisplay);
 				return 'R'
 			elif yCount==4:
-				print("Yellow wins horizontal")
+				PrintIfEnabled("Yellow wins horizontal", gameDisplay)
 				return 'Y'
 
 	# Check horizontals
@@ -84,10 +91,10 @@ def CheckGrid(grid):
 				yCount=0;
 
 			if rCount==4:
-				print("Red wins diagonal (down right)")
+				PrintIfEnabled("Red wins diagonal (down right)", gameDisplay)
 				return 'R'
 			elif yCount==4:
-				print("Yellow wins diagonal (down right)")
+				PrintIfEnabled("Yellow wins diagonal (down right)", gameDisplay)
 				return 'Y'
 
 	upLeft = [[3,0], [4,0], [5,0], [5,1], [5,2], [5,3]]
@@ -111,44 +118,80 @@ def CheckGrid(grid):
 				yCount=0;
 
 			if rCount==4:
-				print("Red wins diagonal (up left)");
+				PrintIfEnabled("Red wins diagonal (up left)", gameDisplay);
 				return 'R'
 			elif yCount==4:
-				print("Yellow wins diagonal (up left)")
+				PrintIfEnabled("Yellow wins diagonal (up left)", gameDisplay)
 				return 'Y'
 
 
-	return 1
+	# Check if board is full
+	for c in range(0, 7):
+		if grid[0, c] == '*':
+			return 1
+
+	return 'T'
 
 
 
 def BestPlay(grid):
 	for c in range (0, 7):
 		g = np.copy(grid)
-		Place('R', c, g)
-		if CheckGrid(g) == 'R':
+		Place('R', c, g, gameDisplay)
+		if CheckGrid(g, gameDisplay) == 'R':
 			return c;
+	
 	return randint(0,6)
 	
 
+# Plays game. 
+# Return 'Y' for yellow win and 'R' for red (AI) win
+# Param gameDisplay: If information about the game should be printed
+def PlayGame(gameDisplay):
+	# Create Grid
+	grid = np.repeat('*', 7*6).reshape(6, 7)
 
-# TODO - init function
-# Create Grid
-grid = np.repeat('*', 7*6).reshape(6, 7)
+	# Play game
+	turn = 'R'
+	if gameDisplay:
+		PrintIfEnabled(grid, gameDisplay)
+		PrintIfEnabled('\n', gameDisplay)
+	gameWinner = 1		# Who the game was won by..... A 1 will indicate the game is still being played.. This is updated in loop
+	while (gameWinner== 1):
+		if turn == 'R':
+			Place(turn, BestPlay(grid), grid, gameDisplay)
+		else:
+			while(Place(turn, randint(0,6), grid, gameDisplay) == 0):
+				pass
 
-# Play game
-turn = 'R'
-print(grid, '\n')
-while (CheckGrid(grid) == 1):
-	if turn == 'R':
-		Place(turn, BestPlay(grid), grid)
+		gameWinner = CheckGrid(grid, gameDisplay)
+
+		if gameDisplay:
+			PrintIfEnabled(grid, gameDisplay)
+			PrintIfEnabled('\n', gameDisplay)
+
+		# Update turn
+		if turn == 'R':
+			turn = 'Y'
+		else:
+			turn = 'R'
+
+	return gameWinner
+
+
+numberOfTurns = 1000
+rWins = 0
+yWins = 0
+tieCount = 0
+gameDisplay = False		# If the game should print boards and info
+
+for i in range (1, numberOfTurns+1):
+	winner = PlayGame(gameDisplay)
+	if winner == 'R':
+		rWins += 1
+	elif winner == 'Y':
+		yWins += 1
 	else:
-		while(Place(turn, randint(0,6), grid) == 0):
-			pass
+		tieCount += 1
 
-	print(grid, '\n')
-
-	if turn == 'R':
-		turn = 'Y'
-	else:
-		turn = 'R'
+print ("R win ratio: ", rWins, " to ", numberOfTurns-tieCount, " tie count:", tieCount)
